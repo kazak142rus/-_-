369158993cc99182422fb29c7f4327f98e14382c
Grogo <n89000547345@yandex.ru>
2024-02-16 19:30:31 +0700

    add st ver t4

---
+++ t4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход 
+// принимает натуральное число N, а на выходе 
+// показывает его цифры через запятую, в обратном порядке.
+
+Console.WriteLine("Введите любое натуральное число.");
+int num = Convert.ToInt32(Console.ReadLine());
+int result =0;
+while (num >10){
+    result = num%10;
+    Console.Write($"{result},");
+    num/=10;
+}
+num%=10;
+Console.Write($"{num}.");
